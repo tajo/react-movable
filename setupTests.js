e77f6a122b3b96c6c6d4153142fbbc6d565895eb
tajo <vojtech@miksu.cz>
2019-01-27 20:51:34 -0800

    increase snapshot threshold again

--- setupTests.js
+++ setupTests.js
@@ -1,6 +1,6 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
-const customConfig = { threshold: 0.02 };
+const customConfig = { threshold: 0.05 };
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffConfig: customConfig
 });
