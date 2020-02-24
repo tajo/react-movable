116345afaec9c0dbfef83914090eb815f248d709
lopesc <lopesc@users.noreply.github.com>
2020-02-24 15:21:53 +0000

    fix(types): Add IItemProps and IProps to exported types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import List from './List';
 import { arrayMove, arrayRemove } from './utils';
+import { IItemProps, IProps } from './types';
 
-export { List, arrayMove, arrayRemove };
+export { List, arrayMove, arrayRemove, IItemProps, IProps };
