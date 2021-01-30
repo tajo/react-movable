95e41d0202559ec670068c0856861b839174945a
Vojtech Miksu <vojtech@uber.com>
2021-01-29 19:49:02 -0700

    add timeouts in e2e tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,6 +3,6 @@ const IS_DEV = process.env.E2E === 'dev';
 module.exports = {
   launch: {
     headless: !IS_DEV,
-    slowMo: IS_DEV ? 200 : 0
+    slowMo: IS_DEV ? 200 : 20
   }
 };
--- setupTests.js
+++ setupTests.js
@@ -1,13 +1,8 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 
-const IS_DEV = process.env.E2E === 'dev';
-
-// in the headless (dev) mode font rendering is slightly different
-// so we need higher threshold
-// in CI, it's a perfect match, thus 0.01
 // you should always save/update snapshots via
 // yarn test:e2e / yarn test:e2e:update
-const customConfig = { threshold: IS_DEV ? 0.1 : 0.01 };
+const customConfig = { threshold: 0.1 };
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
   customDiffConfig: customConfig
 });
